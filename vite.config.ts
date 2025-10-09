71a0335ace9c45ddc5ecf3acecf94d7f9204463f
teentruefx-eng <teentruefx@gmail.com>
2025-10-09 20:58:23 +0330

    build for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/dubai-direct-uae/", // 👈 اضافه شد برای GitHub Pages
+export default defineConfig({
+  base: "/dubai-direct-uae/",
+
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 
   server: {
     host: "::",
     port: 8080,
   },
 
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
