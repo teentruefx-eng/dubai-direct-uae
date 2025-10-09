7aa1e388ea285f754bdd351891aa514deacd88c6
teentruefx-eng <teentruefx@gmail.com>
2025-10-09 21:21:50 +0330

    fix: correct base path for assets

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/dubai-direct-uae/",
-
-  build: {
-    outDir: "docs",
-    emptyOutDir: true,
-  },
-
+  base: "/dubai-direct-uae/", // 👈 مسیر درست برای GitHub Pages
   server: {
     host: "::",
     port: 8080,
   },
-
   plugins: [react()],
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
